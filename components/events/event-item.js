4b0529723a61c0f2bbf502e2c306a318cba5f868
tmcamillo <imeio.tmc@gmail.com>
2022-10-24 18:17:35 +0200

    icons folder added and icons applied

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,8 @@
 import  Button  from '../ui/button'
+import AddressIcon from '../icons/address-icon'
+import DateIcon from '../icons/date-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
+
 import classes from './event-item.module.css'
 
 function EventItem(props) {
@@ -14,14 +18,19 @@ function EventItem(props) {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Button link={exploreLink}>Explore Event</Button>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}><ArrowRightIcon /></span>
+                    </Button>
                 </div>
             </div>
         </li>
