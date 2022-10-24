b3ad49fbe9e1a3de73f2fad875e5508674ccb34c
tmcamillo <imeio.tmc@gmail.com>
2022-10-24 20:01:20 +0200

    navbar created and content included as main on _app page

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import MainHeader from "./main-header";
+
+function Layout(props) {
+    const { children } = props
+    return (
+        <>
+            <MainHeader />
+            <main>{children}</main>
+        </>
+    )
+}
+export default Layout;
