8ccedc8bcad8b87c8ce70486c5c1bfcc48dc3dd3
tmcamillo <imeio.tmc@gmail.com>
2022-10-24 19:50:05 +0200

    structure and layout add to eventDetailPage

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,12 +1,32 @@
 import { useRouter } from "next/router";
+import { getEventById } from "../../data/dummy-data";
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 function EventDetailPage() {
     const route = useRouter();
+    const eventId = route.query.eventId;
+    const event = getEventById(eventId);
+
+    if(!event) {
+        return <p>No event found!</p>
+    }
 
     return (
-        <div>
-            <h1>{`Event Detail ${route.query.eventId}`}</h1>
-        </div>
+        <>
+           <EventSummary title={event.title}/>
+           <EventLogistics 
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+
+            />
+           <EventContent>
+                <p>{event.description}</p>
+           </EventContent>
+        </>
     )
 };
 
